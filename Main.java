6ca73bb8ae08d45a6db3f5ba30784e7a10c6459a
sinbrive <21102151+sinbrive@users.noreply.github.com>
2022-01-12 09:24:55 +0000

    v0.0

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Main {
+  public static void main(String[] args) throws java.lang.Exception {
+    Scanner scanner = new Scanner(System.in);
+    String sc = "";
+    while (!sc.startsWith("00")) {
+      sc = scanner.nextLine();
+      System.out.println(sc);
+    }
+    scanner.close();
+  }
+}
